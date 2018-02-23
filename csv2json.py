2ac54e9b3e6f3fea4d9cf51b992888cf67b991a2
Avinash Anantharamu <anantharamu.avinash@yahoo.com>
2018-02-22 21:20:38 -0800

    Create csv2json.py

---
+++ csv2json.py
@@ -0,0 +1,20 @@
+#python csv2json.py <csv> 
+import csv
+import sys
+import json
+#EDIT THIS LIST WITH YOUR REQUIRED JSON KEY NAMES
+fieldnames=["Title1","Title2"]
+def convert(filename):
+  csv_filename = filename[0]
+  print "Opening CSV file: ",csv_filename
+  f=open(csv_filename, 'r')
+  csv_reader = csv.DictReader(f,fieldnames)
+  json_filename = csv_filename.split(".")[0]+".json"
+  print "Saving JSON to file: ",json_filename
+  jsonf = open(json_filename,'w')
+  data = json.dumps([r for r in csv_reader])
+  jsonf.write(data)
+  f.close()
+  jsonf.close()
+if __name__=="__main__":
+  convert(sys.argv[1:])
